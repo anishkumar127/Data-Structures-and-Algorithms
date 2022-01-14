53b88d79926afdf593b66900209b86488b514b7f
anish <anishbishnoi127@gmail.com>
2022-01-14 13:29:19 +0530

    update files logs

---
+++ Tree/Tree/OLDBinaryTreeCreate.java
@@ -0,0 +1,26 @@
+//package com.company.Trees;
+//
+//class NodeTree{
+//    int data;
+//    NodeTree left;
+//    NodeTree right;
+//
+//}
+//class BinaryTree{
+//    public NodeTree createNewNode(int val){
+//        NodeTree a = new NodeTree();
+//        a.data=val;
+//        a.left=null;
+//        a.right=null;
+//        return a;
+//    }
+//}
+//public class OLDBinaryTreeCreate {
+//    BinaryTree a = new BinaryTree();
+//
+//    NodeTree root = a.createNewNode(2);
+//    root.left = a.createNewNode(7);
+//    root.right=a.createNewNode(5);
+//    root.left.left=a.createNewNode(2);
+//    root.left.right=a.createNewNode(6);
+//}
