7240135ba195dbfb67136ae442db544ef17f2abe
anish <anishbishnoi127@gmail.com>
2022-06-27 15:12:07 +0530

    try changes

--- my practice/Two Sum Array O(n^2).java
+++ my practice/Two Sum Array O(n^2).java
@@ -34,7 +34,19 @@ public class Main {
             System.out.print(i+" "+j);
           }
                 */
-          
+          //        OR
+
+          /******************************************
+
+          for(int i=0; i<nums.length; i ++){
+        for(int j=0; j<nums.length-1; j++){
+          if(nums[i] +nums[j+1]==target){
+            System.out.print(i+" ");
+          }
+        }
+      }
+          ********************************* */
+
             }
         }
     }
