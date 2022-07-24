eb94aaa31daa2b47a28cca45dfd003d48ea55bc2
anish <anishbishnoi127@gmail.com>
2022-07-24 13:29:57 +0530

    print all index occurance of elements.

---
+++ src/Java/Random/Recursion/print all occurance using recusion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+// 1 3 output. print all occupance
+public class Main {
+    public static void main(String[] args) {
+   int arr[] = {1,2,3,2,3,1};
+   int s = 2;
+   output(arr,0,s);
+  }
+  private static void output(int arr[], int i, int s){
+    if(i==arr.length) return;
+    if(arr[i]==s) System.out.print(i+" ") ;
+     output(arr,i+1,s);
+  }
+}
