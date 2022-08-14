c6d7958a6aef315043d6570c615e97d89b31e8ee
anish <anishbishnoi127@gmail.com>
2022-08-14 19:06:26 +0530

    leader in array.

--- src/SELF/week 1/Array Sum print  Using prefix sum.java
+++ src/SELF/week 1/Array Sum print  Using prefix sum.java
@@ -18,7 +18,13 @@ public class Main {
         System.out.print(Arrays.toString(nums));
         
         /*
-        
+         
+       i=0 --> 1;  1 will be there it will not modifiying. 
+       when i start from 1. then array will start modifiying.
+       it will do sum. previous value 
+       i=1 --> 1+2 = 3;
+      i=2 --> 3+3 = 6;
+      i=3 --> 6+4 = 10;
         */
        
         // System.out.print(nums[i]); // 3 6 10
