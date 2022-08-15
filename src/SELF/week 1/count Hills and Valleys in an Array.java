634576975de520adf1bc5dc57b55cd548b5afcf8
anish <anishbishnoi127@gmail.com>
2022-08-15 19:23:46 +0530

    count hills and valleys in an array

--- src/SELF/week 1/count Hills and Valleys in an Array.java
+++ src/SELF/week 1/count Hills and Valleys in an Array.java
@@ -50,3 +50,26 @@ public class Main {
         return ans;
 
 
+
+
+// 
+
+class Solution {
+    public int countHillValley(int[] nums) {
+        int r =0;
+        int left = nums[0];
+        for(int i=1; i<nums.length-1; i++){
+            if(nums[i]>nums[i+1] && nums[i]>left){
+                r++;
+                left = nums[i];
+            }
+            else if(nums[i]<nums[i+1] && nums[i]<left){
+                r++;
+                left=nums[i];
+            }
+        }
+        
+        return r++;
+    }
+}
+
