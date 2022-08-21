6d87e5014b1f9d9519ac4e4f35ac4e3bac67be6f
anish <anishbishnoi127@gmail.com>
2022-08-21 15:31:14 +0530

    update

--- src/SELF/week 2/LeetCode/Unique Email Addresses.java
+++ src/SELF/week 2/LeetCode/Unique Email Addresses.java
@@ -1,3 +1,162 @@
+public int numUniqueEmails(String[] emails) {
+	HashSet<String> uniqueEmails = new HashSet<>(emails.length);
+
+	for (String email : emails) {
+		String sanitizedEmail = "";
+		int indexOfPlusSign = email.indexOf("+");
+		int indexOfAtSign = email.indexOf("@");
+
+		if (indexOfPlusSign > 0) {
+			sanitizedEmail += email.substring(0, indexOfPlusSign);
+		} else {
+			sanitizedEmail += email.substring(0, indexOfAtSign);
+		}
+
+		// removing the dots in the local name
+		sanitizedEmail = sanitizedEmail.replace(".", "");
+		sanitizedEmail += email.substring(indexOfAtSign);
+
+		// HashSet will do the dedup for us
+		uniqueEmails.add(sanitizedEmail);
+	}
+
+	return uniqueEmails.size();
+}
+
+
+
+
+
+
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        // to hold unique emails
+		Set<String> set = new HashSet<>();
+        // loop through available emails
+		for (String email : emails){
+            //to hold temporary processed emails
+			StringBuilder temp = new StringBuilder();
+            int i = 0;
+            //loop until you see '@'
+			while (email.charAt(i) != '@'){
+                char c = email.charAt(i);
+                //if 'c' then increment the index and continue as per the rule
+				if (c == '.'){
+                    i++;
+                    continue;  
+                } 
+                else if (c == '+') break;  // dont have to see the strings further
+                else temp.append(c);  // add to the temporary otherwise
+                i++;
+            }
+            
+            while (email.charAt(i) != '@') i++;  // making sure to move our index to '@'
+            while (i < email.length()) temp.append(email.charAt(i++)); // add to the temporary emails
+            set.add(temp.toString()); // add to the set
+        }
+        return set.size();   // return the size
+    }
+}
+
+
+
+
+Think of the string as 3 separate parts.
+
+Stuff before the first +
+Stuff between the first + and @
+Everything after the @
+Stuff before the first +
+
+Currently we haven't seen '+' yet
+So keep all character as long as it's not '.'
+Stuff between the first + and @
+
+Since we've seen '+', but not '@' yet, don't keep any letters (we don't enter the third if statement so replaceIndex stays the same)
+Everything after the @
+
+Take all characters including '@' and '.'
+How do we keep a letter? We do this by using a char array and maintaining a pointer replaceIndex.
+
+replaceIndex starts at 0 and we increment if we keep the letter
+If we don't keep the letter, replaceIndex stays the same, but the for loop continues iterating
+Next time when we do want to replace, we'll take the current for loop character c and put it into position replaceIndex in the same array
+Take an example like dog.cat@leetcode.com
+
+When we get to index 3 with the '.' char, replaceIndex stays at 3.
+In the next for loop iteration at index 4, we get the letter 'c'
+We replace index 3 which has the '.' with the letter 'c' so the char array will be dogccat@leetcode.com
+Eventually we will get in our char array dogcat@leetcode.comm which we convert to a string and use substring() on
+Basically we are building our string one letter at a time, but instead of putting it in a new array, we're reusing the array
+
+d
+do
+dog
+dogc
+dogca
+dogcat
+dogcat@ etc...
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<String>();
+        
+        for(String email : emails) {
+            char[] chars = email.toCharArray();
+            int n = chars.length;
+            int replaceIndex = 0;
+            
+            boolean seenAt = false;
+            boolean seenPlus = false;
+            
+            for(int i = 0; i < n; i++) {
+                char c = chars[i];
+				
+                if(c == '@') {
+                    seenAt = true;
+                }
+                if(c == '+') {
+                    seenPlus = true;
+                }
+                if(seenAt || (!seenPlus && c != '.')) {
+                    chars[replaceIndex] = c;
+                    replaceIndex++;
+                }
+            }
+            String s = String.valueOf(chars);
+            set.add(s.substring(0, replaceIndex));
+        }
+        return set.size();
+    }
+}
+
+
+
+class Solution {
+    public int numUniqueEmails(String[] emails) 
+    {
+        HashSet<String> hset = new HashSet<>();
+        for(String email:emails)
+        {
+            StringBuilder s = new StringBuilder();
+            boolean flag = true;
+            for(int i=0;i<email.length();i++)
+            {
+                if(email.charAt(i)=='+')
+                    flag = false;
+                else if(email.charAt(i)!='.' && flag==true && email.charAt(i)!='@')
+                    s.append(email.charAt(i));
+                if(email.charAt(i)=='@')
+                {
+                    s.append(email.substring(i));
+                    break;
+                }
+            }
+            hset.add(s.toString());
+        }
+        return hset.size();
+    }
+}
+
 
 
 class Solution {
