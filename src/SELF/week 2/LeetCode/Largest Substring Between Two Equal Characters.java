9db49e67f719ab71bbfccebc72d94e5f4ae90558
anish <anishbishnoi127@gmail.com>
2022-08-22 14:41:13 +0530

    updated

--- src/SELF/week 2/LeetCode/Largest Substring Between Two Equal Characters.java
+++ src/SELF/week 2/LeetCode/Largest Substring Between Two Equal Characters.java
@@ -129,6 +129,21 @@ public int maxLengthBetweenEqualCharacters(String s) {
     }
 
 
+/**
+s.charAt(i) -'a' is the the number of a letter in alphabet order
+for example 'z'-'a' is the 26 which is the last letter in alphabet
+in ther word s.charAt(i) -'a' is the order of s.charAt(i) in alphabet order
+
+ */
+public int maxLengthBetweenEqualCharacters(String s) {
+	int[] arr = new int[26];
+	int max = -1;
+	for(int i = 0 ; i < s.length() ; i++) arr[s.charAt(i) - 'a'] = i; 
+	for(int i = 0 ; i < s.length() ; i++) max = Math.max((arr[s.charAt(i) -'a'] - i - 1),max);
+	return max;
+}
+
+
 // theory part.
 
 
