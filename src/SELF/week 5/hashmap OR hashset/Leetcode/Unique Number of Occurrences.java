18178ac640e9d3a3fd0ac559dc408c9d5ba7c7bb
anish <anishbishnoi127@gmail.com>
2022-09-15 17:57:02 +0530

    Unique Number of Occurrences

--- src/SELF/week 5/hashmap OR hashset/Leetcode/Unique Number of Occurrences.java
+++ src/SELF/week 5/hashmap OR hashset/Leetcode/Unique Number of Occurrences.java
@@ -24,3 +24,33 @@ class Solution {
     }
 }
 
+
+
+3ms
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        
+        int n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        // put value inside hashmap.
+        
+        for(int i=0; i<n; i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        
+        // using hash set for unique value.
+        HashSet<Integer> set = new HashSet<>();
+        
+        // put map value inside hashset. using forEach loop.
+        // make decision earlier.
+        for(int el :map.values()){
+            if(!set.add(el)){       // if no duplicate then put into set.
+                return false;     // if found duplicate return false. keep putting until not duplicate found.
+            }
+        } 
+        
+        return true;  //if all unique return true.
+    }
+}
