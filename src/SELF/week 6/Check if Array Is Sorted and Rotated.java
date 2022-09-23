88c31c00ac8b339ebac6aa5781c7b1b98e947919
anish <anishbishnoi127@gmail.com>
2022-09-23 23:30:33 +0530

    rotate array check

--- src/SELF/week 6/Check if Array Is Sorted and Rotated.java
+++ src/SELF/week 6/Check if Array Is Sorted and Rotated.java
@@ -65,3 +65,19 @@ class Solution {
     }
 }
 ```
+
+
+class Solution {
+    public boolean check(int[] nums) {
+        int count=0;
+       for(int i=1; i<nums.length; i++){
+          if(nums[i-1]>nums[i]){  // if 1st element greater than next element.
+           count++;
+          } 
+       }
+       if(nums[nums.length-1]>nums[0]){  // if last element greater than. first element. 
+               count++;
+           }
+        return count>1?false:true;
+    }
+}
