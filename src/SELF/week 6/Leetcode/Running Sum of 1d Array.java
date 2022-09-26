596dc7e12427b65267143687af96d6e2be294465
Anish Kumar <anishbishnoi127@gmail.com>
2022-09-26 17:45:18 +0530

    prefix sum

--- src/SELF/week 6/Leetcode/Running Sum of 1d Array.java
+++ src/SELF/week 6/Leetcode/Running Sum of 1d Array.java
@@ -12,3 +12,35 @@ class Solution {
     }
 }
 
+
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        int [] output= new int [n];
+        int sum =0;
+        for(int i=0; i<n; i++){
+           sum = sum+ nums[i]; 
+            nums[i] = sum;
+        }
+        
+        return nums;
+    }
+}
+
+
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        int [] output= new int [n];
+        int sum =0;
+        for(int i=0; i<n; i++){
+           nums[i] = nums[i]+sum; 
+            sum=nums[i];
+        }
+        
+        return nums;
+    }
+}
+
