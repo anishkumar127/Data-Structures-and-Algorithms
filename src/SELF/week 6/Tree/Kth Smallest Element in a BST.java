356d23ee24722f8b0d2806a9aae1e7a5b9f1525f
Anish Kumar <anishbishnoi127@gmail.com>
2022-09-26 21:10:38 +0530

    different 3 approach

---
+++ src/SELF/week 6/Tree/Kth Smallest Element in a BST.java
@@ -0,0 +1,130 @@
+# [230\. Kth Smallest Element in a BST](https://leetcode.com/problems/kth-smallest-element-in-a-bst/)
+
+## Description
+
+Difficulty: **Medium**  
+
+Related Topics: [Tree](https://leetcode.com/tag/tree/), [Depth-First Search](https://leetcode.com/tag/depth-first-search/), [Binary Search Tree](https://leetcode.com/tag/binary-search-tree/), [Binary Tree](https://leetcode.com/tag/binary-tree/)
+
+
+Given the `root` of a binary search tree, and an integer `k`, return _the_ k<sup>th</sup> _smallest value (**1-indexed**) of all the values of the nodes in the tree_.
+
+**Example 1:**
+
+![](https://assets.leetcode.com/uploads/2021/01/28/kthtree1.jpg)
+
+```
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+```
+
+**Example 2:**
+
+![](https://assets.leetcode.com/uploads/2021/01/28/kthtree2.jpg)
+
+```
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+```
+
+**Constraints:**
+
+*   The number of nodes in the tree is `n`.
+*   1 <= k <= n <= 10<sup>4</sup>
+*   0 <= Node.val <= 10<sup>4</sup>
+
+**Follow up:** If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+
+
+## Solution
+
+Language: **Java**
+
+```java    TC O(n) sc O(n);
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+      ArrayList<Integer> ans = new ArrayList<>(); 
+        Inorder(root,ans);
+        // Collections.sort(ans, Collections.reverseOrder()); // no need sorting in inorder , if do other then needed.
+        int res = ans.get(k-1);
+        return res;
+    }
+    public void Inorder(TreeNode root, ArrayList<Integer>list){
+        if(root==null) return;
+        Inorder(root.left,list);
+        list.add(root.val);
+        Inorder(root.right,list);
+    }
+}
+```
+
+
+
+optimal 
+
+class Solution {
+    int ans =-1;
+    int count =1;
+    public int kthSmallest(TreeNode root, int k) {
+       inOrder(root,k);
+        return ans;
+    }
+   
+    public void inOrder(TreeNode root, int k){
+        if(root==null) return;
+        if(count>k)return;
+        inOrder(root.left,k);
+        if(count==k){
+             ans = root.val;
+             count++;
+             return;
+        }else{
+            count++;
+        }
+        inOrder(root.right,k);
+    }
+}
+
+
+
+
+different approach.
+
+
+ 
+class Solution {
+    int ans =-1;
+    int k;
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+       inOrder(root);
+        return ans;
+    }
+   
+    public void inOrder(TreeNode root){
+        if(root==null) return;
+        inOrder(root.left);
+         if(--k==0){
+             ans = root.val;
+             return;
+         }
+        inOrder(root.right);
+    }
+}
+
+
