f924b5e84e8f5e870eb10f2f204e906a0754dbcd
Anish Kumar <anishbishnoi127@gmail.com>
2022-09-27 00:48:05 +0530

    BFS

--- src/SELF/week 6/Tree/GFG/Top View of Binary Tree.java
+++ src/SELF/week 6/Tree/GFG/Top View of Binary Tree.java
@@ -37,3 +37,40 @@ class Solution
 }
 }
 
+
+Given below is a binary tree. The task is to print the top view of binary tree. Top view of a binary tree is the set of nodes visible when the tree is viewed from the top. For the given below tree
+
+       1
+    /     \
+   2       3
+  /  \    /   \
+4    5  6   7
+
+Top view will be: 4 2 1 3 7
+Note: Return nodes from leftmost node to rightmost node.
+
+Example 1:
+
+Input:
+      1
+   /    \
+  2      3
+Output: 2 1 3
+Example 2:
+
+Input:
+       10
+    /      \
+  20        30
+ /   \    /    \
+40   60  90    100
+Output: 40 20 10 30 100
+Your Task:
+Since this is a function problem. You don't have to take input. Just complete the function topView() that takes root node as parameter and returns a list of nodes visible from the top view from left to right.
+
+Expected Time Complexity: O(NlogN)
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ Node Data ≤ 105
