dff8284525ea07d3745a8bf5454c8429cd53fdd7
Anish Kumar <anishbishnoi127@gmail.com>
2022-09-27 16:01:38 +0530

    XOR , hashmap, hashset, sorting

--- src/SELF/week 6/Leetcode/Bit Manipulation/Single Number.java
+++ src/SELF/week 6/Leetcode/Bit Manipulation/Single Number.java
@@ -43,7 +43,7 @@ Output: 1
 
 Language: **Java**
 
-```java
+```java  xOR TC O(n) sc O(1);
 class Solution {
     public int singleNumber(int[] nums) {
         int xor =0;
@@ -54,3 +54,71 @@ class Solution {
     }
 }
 ```
+
+
+Approach 1 = Using HashSet ==> TC = O(n) SC=O(n)
+
+class Solution {
+	public int singleNumber(int[] nums) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		for(int i : nums) {
+			if(set.contains(i)) {
+				set.remove(i);
+			} else{
+				set.add(i);
+			}
+		}
+
+		for(int i:set) {
+			return i;
+		}
+		return -1;
+	}
+}
+
+Approach 2 = Using Sorting ==> TC: O(NlogN) SC: O(1)
+
+class Solution {
+	public int singleNumber(int[] nums) {
+		Arrays.sort(nums);
+		for(int i = 1; i < nums.length; i+=2) {
+			if(nums[i] != nums[i-1]) {
+				return nums[i-1];
+			}
+		}
+		return nums[nums.length-1];
+	}
+}
+
+First Approach by using hashMap :-
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> hash = new<Integer, Integer> HashMap();
+        
+        for(int x : nums){
+            if(hash.containsKey(x)){       //if hashmap contains 'element', then increase correspoding val
+                int count = hash.get(x);
+                hash.put(x, ++count);
+            }
+            else
+                hash.put(x, 1);           //if hashmap does not contain  'element', then put it in hashmap
+        }
+        
+        for(int x : hash.keySet()){       //Iterate through 'keys' of HashMap
+            if(hash.get(x)==1)            //if 'value' for 'key' is '1', then return that 'key' element
+                return x;
+        }
+        
+        return -1;
+    }
+}
+Time Complexity : O(nlogn) , as there is a for loop and inside for loop 'hash.containsKey'
+Space Complexity : O(n) , as HashMap of size nearly 'n/2' is used.
+
+
+Why is the time complexity of First Method (HashMap) O(nlogn)? Isn't containsKey in HashMap linear as well?
+
+The time complexity of containsKey has changed in JDK-1.8, as others mentioned it is O(1) in ideal cases. However, in case of collisions where the keys are Comparable, bins storing collide elements aren't linear anymore after they exceed some threshold. In other word, TreeNodes will be used (similar to those in TreeMap) to store bins, (ie: a Red-Black tree structure) and this leaves us with an O(lgn) complexity in-case of collisions.
+
+Please refer the link :- https://stackoverflow.com/a/34545978
