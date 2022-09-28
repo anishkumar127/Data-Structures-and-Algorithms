3e7a8a2a15ead87cfdedf976be0a4b7b39a5fb4f
Anish Kumar <anishbishnoi127@gmail.com>
2022-09-28 17:29:58 +0530

    set bits & >>

---
+++ src/SELF/week 6/GFG/Bit Manipulation/Number of 1 Bits.java
@@ -0,0 +1,40 @@
+class Solution {
+    static int setBits(int N) {
+        int count=0;
+        while(N>0){
+            int bit = N&1;
+            if(bit==1) count++;
+            N = N>>1;
+        }
+        return count;
+    }
+}
+
+Given a positive integer N, print count of set bits in it. 
+
+Example 1:
+
+Input:
+N = 6
+Output:
+2
+Explanation:
+Binary representation is '110' 
+So the count of the set bit is 2.
+Example 2:
+
+Input:
+8
+Output:
+1
+Explanation:
+Binary representation is '1000' 
+So the count of the set bit is 1.
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function setBits() which takes an Integer N and returns the count of number of set bits.
+
+Expected Time Complexity: O(LogN)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 109
