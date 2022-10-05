55a2178e15a63ade6de6ec5bc6545ac1b52d3a7f
Anish Kumar <anishbishnoi127@gmail.com>
2022-10-05 17:21:15 +0530

    1D array sort os 1s 2s

---
+++ src/SELF/week 8/Array/GFG/Sort an array of 0s, 1s and 2s.java
@@ -0,0 +1,72 @@
+class Solution
+{
+    public static void sort012(int arr[], int n)
+    {
+        int zero=0 , one =0 , two=0;
+        for(int value:arr){
+            if(value==0){
+                zero++;
+            }else if(value==1){
+                one++;
+            }else{
+                two++;
+            }
+        } 
+        
+        int index=0;
+        while(zero-->0){
+            arr[index++]=0;
+        }
+        
+        while(one-->0){
+            arr[index++]=1;
+        }
+        while(two-->0){
+            arr[index++]=2;
+        }
+        
+    }
+}
+
+
+
+
+
+
+// theory
+
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+Example 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+Example 2:
+
+Input: 
+N = 3
+arr[] = {0 1 0}
+Output:
+0 0 1
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 <= N <= 10^6
+0 <= A[i] <= 2
