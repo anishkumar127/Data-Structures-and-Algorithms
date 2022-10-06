15b8a213b447c81e6b2d2f1a77898188a961cd3f
Anish Kumar <anishbishnoi127@gmail.com>
2022-10-07 00:25:15 +0530

    tips

--- Interview Ready Solution/Preparation 🔥/Tips  & Help/Converting ArrayList to Array in java.java
+++ Interview Ready Solution/Preparation 🔥/Tips  & Help/Converting ArrayList to Array in java.java
@@ -45,3 +45,102 @@ int[] intArray = Ints.toArray(listOfIntegers);
 
 
 
+
+
+
+//
+Convert the Java List into Array
+
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList languages = new ArrayList<>();
+
+    // Add elements in the list
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("JavaScript");
+    System.out.println("ArrayList: " + languages);
+
+    // Create a new array of String type
+    String[] arr = new String[languages.size()];
+
+    // Convert ArrayList into the string array
+    languages.toArray(arr);
+    System.out.print("Array: ");
+    for(String item:arr) {
+      System.out.print(item+", ");
+    }
+  }
+}
+
+
+
+Example 2: Convert Java Array to List
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+class Main {
+  public static void main(String[] args) {
+
+    // create an array
+    String[] array = {"Java", "Python", "C"};
+    System.out.println("Array: " + Arrays.toString(array));
+
+    // convert array to list
+    List languages= new ArrayList<>(Arrays.asList(array));
+
+    System.out.println("List: " + languages);
+
+  }
+}
+
+
+
+
+
+
+Example 1: Convert Array to Set
+
+
+import java.util.*;
+
+public class ArraySet {
+
+    public static void main(String[] args) {
+
+        String[] array = {"a", "b", "c"};
+        Set<String> set = new HashSet<>(Arrays.asList(array));
+
+        System.out.println("Set: " + set);
+    }
+}
+
+
+
+Example 3: Convert Set to Array
+
+
+import java.util.*;
+
+public class SetArray {
+
+    public static void main(String[] args) {
+
+        Set<String> set = new HashSet<>();
+        set.add("a");
+        set.add("b");
+        set.add("c");
+
+        String[] array = new String[set.size()];
+        set.toArray(array);
+
+        System.out.println("Array: " + Arrays.toString(array));
+
+    }
+}
+
+
