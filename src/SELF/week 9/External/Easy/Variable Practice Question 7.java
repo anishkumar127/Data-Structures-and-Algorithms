570c0384201f9724588d5217eb6a84b1a7b4ebd9
Anish Kumar <anishbishnoi127@gmail.com>
2022-10-27 22:02:04 +0530

    pi problem
    Co-Authored-By: not-just <114912156+not-just@users.noreply.github.com>

---
+++ src/SELF/week 9/External/Easy/Variable Practice Question 7.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+            int r=10;
+                
+         float pi = 3.14f;
+         // int area =;
+      System.out.println((int)((double)pi*(r*r)));
+      System.out.println((int)(2*pi*r));
+                  
+    }
+}
+
+
+
+Assign an integer value of 10 to a variable "r" and print the area and perimeter of the circle of radius "r".
+
+Assume the value of pi as 3.14.
+
+Input Format
+No input.
+
+Output Format
+In the first line, print the area of the circle of radius r. In the next line print the perimeter of the circle of radius r.
+
+Print only integer part of area and perimeter.
+
+Output:
+
+314
+62
